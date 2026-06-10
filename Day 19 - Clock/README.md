d4b24fc3dd8c0a5bf8eff2adff33fe9ea8e6bbba
NikolaVekic <55920607+NikolaVekic@users.noreply.github.com>
2026-06-10 13:51:11 +0200

    Clock

---
+++ Day 19 - Clock/README.md
@@ -0,0 +1,1 @@
+![Recording2025-04-15205542-ezgif com-video-to-gif-converter](https://github.com/user-attachments/assets/f341405c-5ec4-4fb2-b952-6bd7d52bec48)
---
+++ Day 19 - Clock/script.js
@@ -0,0 +1,86 @@
+const hourElement = document.querySelector(".hour");
+const minuteElement = document.querySelector(".minute");
+const secondElement = document.querySelector(".second");
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
+const toggleElement = document.querySelector(".toggle");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+toggleElement.addEventListener("click", () => {
+  if (toggleElement.innerHTML !== "Dark Mode") {
+    toggleElement.innerHTML = "Dark Mode";
+    document.querySelector("html").classList.remove("dark");
+  } else {
+    toggleElement.innerHTML = "Light Mode";
+    document.querySelector("html").classList.add("dark");
+  }
+});
+
+const setTime = () => {
+  const time = new Date();
+  const month = time.getMonth();
+  const day = time.getDay();
+  const date = time.getDate();
+  const hours = time.getHours();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
+
+  hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+  minuteElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+  secondElement.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  timeElement.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+};
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setInterval(setTime, 1000);
