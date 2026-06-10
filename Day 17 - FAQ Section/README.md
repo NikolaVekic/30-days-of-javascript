699156bd274ae49f370deec6e1e1fc338b477149
NikolaVekic <55920607+NikolaVekic@users.noreply.github.com>
2026-06-10 13:51:11 +0200

    FAQ Section

---
+++ Day 17 - FAQ Section/README.md
@@ -0,0 +1,1 @@
+![Recording2025-04-01181104-ezgif com-crop](https://github.com/user-attachments/assets/e48b05bc-57ee-41ff-ad5d-7003aee5fa22)
---
+++ Day 17 - FAQ Section/script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".faq-toggle");
+
+buttons.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.parentNode.classList.toggle("active");
+  });
+});
